16f99ba8eb65ac0bbc6cb199c51ade64ed94aa07
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-08-15 02:08:21 +0300

    readable filename

--- src/caller.jl
+++ src/caller.jl
@@ -3,12 +3,9 @@ using .Trace
 
 #putting two statement in one line so that linenumer is the same
 println("I want: ", Location(@__FILE__, @__LINE__)); 
-#println(" I get: ", @trace)
+println(" I get: ", @trace)
 
 t = @trace
-f = @file
-#show(f)
-# TODO: converting to 
 
 
 # fieldnames
--- src/trace.jl
+++ src/trace.jl
@@ -1,34 +1,17 @@
-macro file1()
-  return QuoteNode(__source__.file)
-end
-
-f = @file1
-g = String(@file1)
-
-# todo: use embedded macro
-
-
-macro file2()
-  x = QuoteNode(__source__.file)
-  return String(x)
-end
-
-h = @file2
-
-#==
-D:\github\Handout.jl\src>julia trace.jl
-ERROR: LoadError: LoadError: MethodError: no method matching String(::QuoteNode)
-Closest candidates are:
-  String(!Matched::String) at boot.jl:318
-  String(!Matched::Array{UInt8,1}) at strings/string.jl:39
-  String(!Matched::Base.CodeUnits{UInt8,String}) at strings/string.jl:77
-  ...
-Stacktrace:
- [1] @file2(::LineNumberNode, ::Module) at D:\github\Handout.jl\src\trace.jl:13
-in expression starting at D:\github\Handout.jl\src\trace.jl:16
-in expression starting at D:\github\Handout.jl\src\trace.jl:16
-
-=#
-
-#end
+# Following:
+# 
 # https://discourse.julialang.org/t/tracing-and-saving-line-number-of-a-funtcion-call/27488
+# 
+module Trace
+export @trace, Location
+
+struct Location
+    file:: String
+    line:: Integer
+end
+
+macro trace()
+    return Location(String(__source__.file), __source__.line)
+end    
+
+end
