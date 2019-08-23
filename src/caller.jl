08150754ad755bfaf63cad041ea8591efaa55645
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-08-24 01:48:28 +0300

    @handout, start of prototype

--- src/caller.jl
+++ src/caller.jl
@@ -1,12 +1,21 @@
 include("trace.jl")
 using .Trace
+using Test
 
 #putting two statement in one line so that linenumer is the same
-println("I want: ", Location(@__FILE__, @__LINE__)); 
-println(" I get: ", @trace)
+println("I want: ", Location(@__FILE__, @__LINE__)); println(" I get: ", @trace)
 
 t = @trace
 
+println("Performing tests...")
+@test t isa Location 
+@test t.line == 8
+@test endswith(t.file, "caller.jl")
+println("Done.")
 
-# fieldnames
-# methodswith(QuoteNode)
+doc = @handout(".", "My report")
+println(doc)
+
+# Note: remember to use
+#   fieldnames()
+#   methodswith()
--- src/trace.jl
+++ src/trace.jl
@@ -3,7 +3,7 @@
 # https://discourse.julialang.org/t/tracing-and-saving-line-number-of-a-funtcion-call/27488
 # 
 module Trace
-export @trace, Location
+export @trace, Location, Handout, @handout
 
 struct Location
     file:: String
@@ -13,5 +13,21 @@ end
 macro trace()
     return Location(String(__source__.file), __source__.line)
 end    
+
+# TODO: passing a directory to Handout as parameter
+
+const FilePath = String
+const DirectoryPath = String
+
+struct Handout
+    script:: FilePath
+    directory:: DirectoryPath
+    title:: String
+end
+
+macro handout(directory, title="Handout")
+    return Handout(String(__source__.file), directory, title)
+end    
+
 
 end
