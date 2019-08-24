29a2c4b20ad92b1b6ffb43f964c97c45d3e310d4
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-08-25 02:09:43 +0300

    global vars inside a loop scope is such a mess

--- src/error.jl
+++ src/error.jl
@@ -1,32 +1,9 @@
-include("Handout.jl")
-using .Handout
-using Test
-
-function merge(doc:: Document, breaks)
-    lines = readlines(doc.script)
-    result = Vector{String}[]
-    as = [0; breaks]
-    bs = [breaks; length(lines)]
-    for (a, b) in zip(as,bs)
-        push!(result, lines[(a+1):b])
-    end    
-    return result
-end   
-
-doc = @handout(".", "My report")
-a = merge(doc, [15, 22])
-
-#=
-Something useful to say.
-In several lines
-=#
-
 # Learn julia:
 # - http://www.stochasticlifestyle.com/type-dispatch-design-post-object-oriented-programming-julia/
 # - can use macros to propagate methods in julia 
 # - append can change inline
 
-lines = a[2]
+using Test 
 
 struct Code
     lines:: Vector{String}
@@ -52,7 +29,7 @@ function to_blocks(lines)
     for line in lines
         # option 1: started comment
         if startswith(line,"#=")
-            block.lines == [] || (blocks = [blocks; block])
+            block.lines == [] || blocks = [blocks; block]
             line = line[3:end]
             block = Text([line])
             continue
@@ -70,7 +47,5 @@ function to_blocks(lines)
     return blocks  
 end  
 
-lines = ["", "doc = @handout(\".\", \"My report\")", "a = merge(doc, [13, 22])", "", "#=", "Something useful to say.", "In several lines", "=#", ""]
+lines =  ["", "doc = @handout(\".\", \"My report\")", "a = merge(doc, [13, 22])", "", "#=", "Something useful to say.", "In several lines", "=#", ""]
 show(to_blocks(lines))
-# need overload equality operator
-# @test to_blocks(lines) == Any[Code(["", "doc = @handout(\".\", \"My report\")", "a = merge(doc, [13, 22])", ""]), Text(["", "Something useful to say.", "In several lines", ""])]
