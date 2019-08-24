b032e4e95c52e778775b2307b662f8df2caca576
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-08-25 02:30:12 +0300

    closer to handout

--- src/dev.jl
+++ src/dev.jl
@@ -50,24 +50,23 @@ function to_blocks(lines)
     blocks = [] # TODO: change to union type
     block = Code([])
     for line in lines
-        # option 1: started comment
+        # option: started comment
         if startswith(line,"#=")
-            block.lines == [] || (blocks = [blocks; block])
+            if block.lines != [] (blocks = [blocks; block]) end
             line = line[3:end]
-            block = Text([line])
-            continue
-        # option 2: ending comment
-        elseif endswith(line, "=#")
+            block = Text([])
+        # option: ended comment, can be same line as started comment
+        if endswith(line, "=#")
             line = line[1:end-2]
             blocks = [blocks; append(block, line)]
             block = Code([])
             continue
-        # option 3: continue previous block    
-        else   
-            block = append(block, line) 
-        end
-    end    
+        # option: continue previous block    
+        block = append(block, line) 
+    end
+    blocks = [blocks; block] # add last block after loop   
     return blocks  
+        
 end  
 
 lines = ["", "doc = @handout(\".\", \"My report\")", "a = merge(doc, [13, 22])", "", "#=", "Something useful to say.", "In several lines", "=#", ""]
