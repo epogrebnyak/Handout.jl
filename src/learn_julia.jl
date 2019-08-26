84e62b5cb2ba79a2ed41e7f2bcc70c26f44ec1cc
Evgeniy <e.pogrebnyak@gmail.com>
2019-08-26 11:10:26 +0300

    julia language notes

---
+++ src/learn_julia.jl
@@ -0,0 +1,11 @@
+# == Julia learning space ==
+
+# greet() = print("Hello World!")
+
+# remember to use fieldnames(), methodswith()
+
+# - http://www.stochasticlifestyle.com/type-dispatch-design-post-object-oriented-programming-julia/
+
+# - Can use macros to propagate methods in julia - see doc example with sin, cos, etc
+
+# - append! will change inline
